71891490af6f1ce0b8a1e568b57d11082cf8442c
Bram KvO <bram@kolffvanoosterwijk.com>
2026-05-07 10:36:45 +0200

    4.4.1.3.4 af

--- MCPizzeriaSQL.py
+++ MCPizzeriaSQL.py
@@ -15,7 +15,32 @@ with sqlite3.connect("MCPizzeria.db") as db:
 
 
 ### ---------  Functie definities  -----------------
+def maakTabellenAan():
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS tbl_pizzas(
+            gerechtID INTEGER PRIMARY KEY AUTOINCREMENT,
+            gerechtNaam TEXT NOT NULL,
+            gerechtPrijs REAL NOT NULL);""")
+    print("tbl_pizzas aangemaakt")
 
+def printTabel(tabel_naam):
+    cursor.execute("SELECT * FROM " + tabel_naam)
+    opgehaalde_gegevens = cursor.fetchall()
+    print("tabel " + tabel_naam + ":", opgehaalde_gegevens)
 
+def voegPizzaToe(naam_nieuwe_pizza, prijs_nieuwe_pizza):
+    cursor.execute("INSERT INTO tbl_pizzas VALUES(NULL, ?, ? )", (naam_nieuwe_pizza, prijs_nieuwe_pizza))
+    db.commit()
+
+    #print("pizza toegevoegd:")
+    #printTabel("tbl_pizzas")
+
+def verwijderPizza(gerechtNaam):
+    cursor.execute("DELETE FROM tbl_pizzas WHERE gerechtNaam = ?", (gerechtNaam,))
+    print("Gerecht verwijderd uit 'tbl_pizzas':", gerechtNaam )
+    db.commit()
 ### --------- Hoofdprogramma  ---------------
 
+maakTabellenAan()
+verwijderPizza("Hawaii")
+printTabel("tbl_pizzas")
